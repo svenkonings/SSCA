c61eed5e2b34455e4ff4f1f58a70a512387ed9eb
Sven Konings <svenkonings@live.nl>
2020-02-04 14:52:30 +0100

    Update Scalastyle for new sbt version

--- codeAnalysis/src/main/scala/codeAnalysis/Main.scala
+++ codeAnalysis/src/main/scala/codeAnalysis/Main.scala
@@ -9,10 +9,10 @@ import main.scala.analyser.Analyser
 object Main {
 
   def main (args: Array[String] ): Unit = {
-    val a = "C:\\Users\\ErikL\\IdeaProjects\\SSCA\\codeAnalysis\\src\\main\\scala\\codeAnalysis\\Utils\\FunctionalUtil.scala"
+    val a = "C:\\Users\\SvenK\\Documents\\SSCA\\codeAnalysis\\src\\main\\scala\\codeAnalysis\\Utils\\FunctionalUtil.scala"
 
     val metrics = List(new Complex)
-    val an = new Analyser(metrics, "C:\\Users\\ErikL\\IdeaProjects\\SSCA\\codeAnalysis", 1)
+    val an = new Analyser(metrics, "C:\\Users\\SvenK\\Documents\\SSCA\\codeAnalysis", 1)
 
     STimer.time("Analyse", println(an.analyse(a)))
 
